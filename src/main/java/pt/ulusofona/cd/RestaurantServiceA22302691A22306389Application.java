c00718100b75d3bdc38a8f968480af719257a8dc
Rodrigo-Nunes-a22302691 <a22302691@alunos.ulht.pt>
2025-12-19 17:32:47 +0000

    Inicio

---
+++ src/main/java/pt/ulusofona/cd/RestaurantServiceA22302691A22306389Application.java
@@ -0,0 +1,13 @@
+package pt.ulusofona.cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantServiceA22302691A22306389Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantServiceA22302691A22306389Application.class, args);
+    }
+
+}
---
+++ src/test/java/pt/ulusofona/cd/RestaurantServiceA22302691A22306389ApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.ulusofona.cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantServiceA22302691A22306389ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
